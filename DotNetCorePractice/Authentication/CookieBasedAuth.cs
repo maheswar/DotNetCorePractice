ee2c00b973d39d3d1fc3dc172a0fbd2a8ddcae2f
maheswar <maheswarreddy4@gmail.com>
2020-03-04 23:21:57 +0530

    Test

---
+++ DotNetCorePractice/Authentication/CookieBasedAuth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCorePractice.Authentication
+{
+    public class CookieBasedAuth
+    {
+    }
+}
